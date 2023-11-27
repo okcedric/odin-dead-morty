5c3f43916e08b6810e224fba55910c6b4d5a7894
OKCEDRIC <contact@okcedric.com>
2023-11-27 14:10:18 +0100

    Remove the hover effect that persisted on touch

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,12 +1,23 @@
 import './Card.css'
 function Card({ morty, handleTouch }) {
   let image = morty.image;
+  let divId = 'card-'+morty.id;
+  let div = document.getElementById(divId);
 
   return (
     <div
+      id={divId}
       key={morty.id}
-      className="card"
+      className="card hoverable"
       onClick={() => handleTouch(morty.id)}
+      //remove the hoverable effect when touched
+      onTouchStart={() =>
+        document.getElementById(divId).classList.remove("hoverable")
+      }
+      //add the hoverable effect when mouse is moving
+      onMouseLeave={() =>
+        document.getElementById(divId).classList.add("hoverable")
+      }
     >
       <div className="image" style={{ backgroundImage: `url(${image})` }}></div>
       <h4 className="name">{morty.name}</h4>
